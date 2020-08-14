58d893c1fed47a8602d23dc1d7251c23aaeba2b5
ltreven <ltreven@gmail.com>
2020-08-14 15:57:04 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const hbs          = require('hbs');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+const cors         = require('cors');
+const app_name     = require('./package.json').name;
+const debug        = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// default value for title local
+app.locals.title = 'Test application for Klarna products';
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:3001', 'http://localhost:3000'] // <== aceptar llamadas desde este dominio
+  })
+);
+
+
+const index = require('./routes/index');
+app.use('/', index);
+
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+module.exports = app;
---
+++ public/precache-manifest.6847b2123ebddfcdff739af94d34f2ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ddf000dc235a04c71b5df47589a780",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd6595712032fbe34100",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2ee5cc13c278d469a208",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd6595712032fbe34100",
+    "url": "/static/js/2.c286a7eb.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.c286a7eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ee5cc13c278d469a208",
+    "url": "/static/js/main.4a1c516a.chunk.js"
+  },
+  {
+    "revision": "108db3b676248f5fd32d",
+    "url": "/static/js/runtime-main.60acddf7.js"
+  }
+]);
