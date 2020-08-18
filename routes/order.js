82c86d68038ada1aea6eaee0ddfa104761cb9d6e
ltreven <ltreven@gmail.com>
2020-08-18 09:23:20 +0200

    place order

--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const router  = express.Router();
+const axios = require("axios");
 
 /* GET home page */
 router.post('/', (req, res, next) => {
-  console.log(req.body)
+  console.log("body received:", req.body)
+  axios.post(`https://api.playground.klarna.com/instantshopping/v1/authorizations/${req.body.authorization_token}/orders`)
+
+  var endpoint = `https://api.playground.klarna.com/instantshopping/v1/authorizations/${req.body.authorization_token}/orders`;
+  var username = 'usePK27126_8a4253efbe80r';
+  var password = 'A6Xt0DcLnyysBoM8';
+  var credentials = btoa(username + ':' + password);
+  var basicAuth = 'Basic ' + credentials;
+  var body = {
+    purchase_country: req.body.purchase_country,
+    purchase_currency: req.body.purchase_currency,
+    locale: req.body.locale,
+    merchant_urls: {
+      confirmation: "https://klarna-test-website.herokuapp.com/terms", // mandatory if not given at step #3, this value will ovewrite the one set through button generation
+    },
+    billing_address: req.body.billing_address,
+    shipping_address: req.body.shipping_address,
+    customer: req.body.customer,
+    order_amount: req.body.order_amount,
+    order_tax_amount: req.body.order_tax_amount,
+    order_lines: req.body.order_lines,
+    merchant_reference1: req.body.merchant_reference1,
+  }
+  axios.post(endpoint, {
+    headers: { 'Authorization': + basicAuth }
+  }, body).then(function(response) {
+
+    console.log('authorizations called for ' + req.body.authorization_token);
+    console.log("response", response)
+
+  }).catch(function(error) {
+    console.log('Error on Authentication');
+  });
+
   res.json({message: "place order method being called"})
 });
 
